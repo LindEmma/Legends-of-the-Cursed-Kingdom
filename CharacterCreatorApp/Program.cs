b8db5ecf686c64c5ef1b5c8ffe0e29d6bd779008
Emma <elune.lind@gmail.com>
2024-06-28 16:16:49 +0200

    comments

--- CharacterCreatorApp/Program.cs
+++ CharacterCreatorApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace CharacterCreatorApp
 
+// Emma Lind .NET23
+// Labb 1 - designmönster och arkitektur
+
 // Abstract factory pattern for creating two families of characters (players and enemies),
 // strategy pattern for choosing an attack strategy
 // & observer makes it possible for the player to choose if they want to view the enemy's health after each round or not.
